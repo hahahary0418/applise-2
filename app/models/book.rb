f44a1f91732c610839dd6ac2b279af141f28b69b
EC2 Default User <ec2-user@ip-172-31-46-218.ap-northeast-1.compute.internal>
2024-09-22 07:35:37 +0000

    applise2

---
+++ app/models/book.rb
@@ -0,0 +1,14 @@
+class Book < ApplicationRecord
+  has_one_attached :profile_image
+  belongs_to :user
+
+
+
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
+end
---
+++ db/migrate/20240922034624_addtitle_to_users.rb
@@ -0,0 +1,4 @@
+class AddtitleToUsers < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
